95bfc9961b892bbdce1d06aa9743ae89fb33fedf
Justin Schow <justin.schow@gmail.com>
2025-08-03 09:21:00 -0600

    Setup basic functions in app.jsx

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ToDoList from "./ToDoList.jsx";
+
 
 export default function App() {
-  return (<></>);
+  return (<ToDoList />);
 }
 
 // function App() {
---
+++ project/frontend/src/ToDoList.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function ToDoList(){
+
+    const [tasks, setTasks] = useState([]);
+    const [task, setTask] = useState("");
+
+
+    function handleInputChange(event) {
+    
+    }
+
+    function AddTask() {
+
+    }
+
+    function deleteTask(index) {
+    
+    }
+    
+    return (<></>); 
+}
