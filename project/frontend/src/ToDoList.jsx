e73e58ccbc7843c64af9b360b9572bd345ee0eed
Justin Schow <justin.schow@gmail.com>
2025-08-03 09:33:30 -0600

    Add placeholder tasks to list

--- project/frontend/src/ToDoList.jsx
+++ project/frontend/src/ToDoList.jsx
@@ -4,10 +4,12 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 export default function ToDoList() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(["Plan the project", "Write the code", "Test the application"]);
   const [task, setTask] = useState("");
 
-  function handleInputChange(event) {}
+  function handleInputChange(event) {
+    setTask(event.target.value);
+  }
 
   function AddTask() {}
 
@@ -15,8 +17,30 @@ export default function ToDoList() {
 
   return (
     <div className="todo-list">
-      <h1>JR's To-Do List</h1>
-      <div></div>
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={task}
+          onChange={handleInputChange}
+        />
+        <button 
+            className="add-task-button"
+            onClick={AddTask}>
+          Add
+        </button>
+      </div>
+
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => deleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+
     </div>
   );
 }
