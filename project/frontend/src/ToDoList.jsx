28dfa0cb35759b615bf73c3044cc5bab8febd925
Justin Schow <justin.schow@gmail.com>
2025-08-03 15:39:12 -0600

    Add edit button

--- project/frontend/src/ToDoList.jsx
+++ project/frontend/src/ToDoList.jsx
@@ -6,6 +6,8 @@ import "./App.css";
 export default function ToDoList() {
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState("");
+  const [editingTaskIndex, setEditingTaskIndex] = useState(null);
+  const [editingTaskText, setEditingTaskText] = useState("");
 
   function handleInputChange(event) {
     setTask(event.target.value);
@@ -21,6 +23,24 @@ export default function ToDoList() {
   function deleteTask(index) {
     const taskList = tasks.filter((element, i) => i !== index);
     setTasks(taskList);
+
+    if (editingTaskIndex === index) {
+      setEditingTaskIndex(null);
+    } else if (editingTaskIndex > index) {
+      setEditingTaskIndex(editingTaskIndex - 1);
+    }
+  }
+
+  function handleStartEdit(index) {
+    setEditingTaskIndex(index);
+    setEditingTaskText(tasks[index]);
+  }
+
+  function handleSaveEdit(event) {
+    const updatedTasks = [...tasks];
+    updatedTasks[editingTaskIndex] = editingTaskText;
+    setTasks(updatedTasks);
+    setEditingTaskIndex(null);
   }
 
   return (
@@ -41,13 +61,43 @@ export default function ToDoList() {
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
-            <span className="text">{task}</span>
-            <button
-              className="delete-task-button"
-              onClick={() => deleteTask(index)}
-            >
-              Delete
-            </button>
+            {editingTaskIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={editingTaskText}
+                  onChange={(e) => setEditingTaskText(e.target.value)}
+                />
+                <button
+                  className="edit-task-button"
+                  onClick={() => handleSaveEdit(index)}
+                >
+                  Done
+                </button>
+                <button
+                  className="delete-task-button"
+                  onClick={() => deleteTask(index)}
+                >
+                  Delete
+                </button>
+              </>
+            ) : (
+              <>
+                <span className="text">{task}</span>
+                <button
+                  className="edit-task-button"
+                  onClick={() => handleStartEdit(index)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="delete-task-button"
+                  onClick={() => deleteTask(index)}
+                >
+                  Delete
+                </button>
+              </>
+            )}
           </li>
         ))}
       </ul>
