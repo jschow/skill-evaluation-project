6e7b91ff5750754f5ab7c2322751a2c56a1e3c81
Justin Schow <justin.schow@gmail.com>
2025-08-03 18:13:01 -0600

    Install axios

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState , useEffect, use } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 import ToDoList from "./ToDoList.jsx";
 
 export default function App() {
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api");
+      setTasks(response.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <ToDoList />;
 }
+
+
